141f092b17a877b96d17f0be1171359cf262d3da
reciokaue <kaue.recio2@gmail.com>
2024-03-13 19:53:20 -0300

    Sign in header

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,6 +1,6 @@
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
-import { Center, Image, Text, VStack } from 'native-base'
+import { Center, Heading, Image, Text, VStack } from 'native-base'
 
 export function Signin() {
   return (
@@ -16,6 +16,11 @@ export function Signin() {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
       </Center>
     </VStack>
   )
