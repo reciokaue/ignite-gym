c42be1dbf9b07ad13348a24df832579402a62140
reciokaue <kaue.recio2@gmail.com>
2024-03-13 19:26:03 -0300

    native base theme

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import { Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { useFonts } from 'expo-font'
 import { StatusBar } from 'expo-status-bar'
 import { NativeBaseProvider, View } from 'native-base'
+import { THEME } from 'src/theme'
 
 export default function App() {
   const [fontsloaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
       {fontsloaded ? <View /> : <Loading />}
     </NativeBaseProvider>
