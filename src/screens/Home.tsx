e9e2d359798e2e69c2a444c2738619b471e7ef29
reciokaue <kaue.recio2@gmail.com>
2024-03-14 18:16:51 -0300

    gtoup list

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,37 @@
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { VStack } from 'native-base'
+import { FlatList, HStack, VStack } from 'native-base'
+import { useState } from 'react'
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    'costas',
+    'ombro',
+    'bíceps',
+    'triceps',
+    'ombro',
+  ])
+  const [selectedGroup, setSelectedGroup] = useState('')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <Group name="Costas" />
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => (
+          <Group
+            key={item}
+            onPress={() => setSelectedGroup(item)}
+            name={item}
+            isActive={selectedGroup === item}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
