894448d2d7c216cca636bd9953a1f8c0d0c8844b
reciokaue <kaue.recio2@gmail.com>
2024-04-01 18:16:27 -0300

    exercise detail screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,7 +1,10 @@
 import BodySvg from '@assets/body.svg'
+import RepetitionsSvg from '@assets/repetitions.svg'
+import SeriesSvg from '@assets/series.svg'
+import { Button } from '@components/Button'
 import { Feather } from '@expo/vector-icons'
 import { useNavigation } from '@react-navigation/native'
-import { Heading, HStack, Icon, Text, VStack } from 'native-base'
+import { Box, Heading, HStack, Icon, Image, Text, VStack } from 'native-base'
 import { TouchableOpacity } from 'react-native'
 
 export function Exercise() {
@@ -34,6 +37,42 @@ export function Exercise() {
             </Text>
           </HStack>
         </HStack>
+      </VStack>
+
+      <VStack p={8}>
+        <Image
+          w="full"
+          h={80}
+          source={{
+            uri: 'https://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg',
+          }}
+          alt="Nome do exercício"
+          mb={3}
+          resizeMode="cover"
+          rounded="lg"
+        />
+        <Box bg="gray.600" rounded="md" px={4} pb={4}>
+          <HStack
+            alignItems="center"
+            justifyContent="space-around"
+            mb={6}
+            mt={5}
+          >
+            <HStack>
+              <SeriesSvg />
+              <Text color="gray.200" ml="2">
+                3 séries
+              </Text>
+            </HStack>
+            <HStack>
+              <RepetitionsSvg />
+              <Text color="gray.200" ml="2">
+                12 repetições
+              </Text>
+            </HStack>
+          </HStack>
+          <Button title="Marcar como realizado" />
+        </Box>
       </VStack>
     </VStack>
   )
