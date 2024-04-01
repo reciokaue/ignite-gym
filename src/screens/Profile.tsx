a2959ca62e6494909e0e06422e69597c023aab75
reciokaue <kaue.recio2@gmail.com>
2024-04-01 17:35:02 -0300

    change photo button

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,16 +1,43 @@
 import { ScreenHeader } from '@components/ScreenHeader'
-import { Avatar, Center, ScrollView, VStack } from 'native-base'
+import { Avatar, Center, ScrollView, Skeleton, Text, VStack } from 'native-base'
+import { useState } from 'react'
+import { TouchableOpacity } from 'react-native'
+
+const photo_size = 33
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
       <ScrollView>
         <Center mt={6} px={10}>
-          <Avatar
-            source={{ uri: 'https://github.com/reciokaue.png' }}
-            size={33}
-          />
+          {photoIsLoading ? (
+            <Skeleton
+              w={photo_size}
+              h={photo_size}
+              rounded="full"
+              startColor="gray.500"
+              endColor="gray.400"
+            />
+          ) : (
+            <Avatar
+              source={{ uri: 'https://github.com/reciokaue.png' }}
+              size={photo_size}
+            />
+          )}
+          <TouchableOpacity>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
+            </Text>
+          </TouchableOpacity>
         </Center>
       </ScrollView>
     </VStack>
