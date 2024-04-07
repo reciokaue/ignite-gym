517df9523b3db3e86a5a9bdcc29da6b46e32913c
reciokaue <kaue.recio2@gmail.com>
2024-04-07 15:17:42 -0300

    auth context user and DTO

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,15 +1,23 @@
-import { createContext, ReactNode, useContext } from 'react'
+import { createContext, ReactNode, useContext, useState } from 'react'
+import { UserDTO } from 'src/DTOs/user'
 
 interface AuthProviderProps {
   children: ReactNode
 }
 
-interface AuthContextData {}
+interface AuthContextData {
+  user: UserDTO
+}
 
 const AuthContext = createContext({} as AuthContextData)
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>
+  const [user, setUser] = useState<UserDTO>({id: '1', name: 'Kaue', email: 'kaue@gmail.com', 'kaue.png'})
+
+  return <AuthContext.Provider value={{
+    user
+
+  }}>{children}</AuthContext.Provider>
 }
 
 export const useAuth = () => useContext(AuthContext)
