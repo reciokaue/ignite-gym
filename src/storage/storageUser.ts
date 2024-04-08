1cb078de8c7bcbf770742e3c78cb5a2555c35c68
reciokaue <kaue.recio2@gmail.com>
2024-04-08 19:07:48 -0300

    storage get user

--- src/storage/storageUser.ts
+++ src/storage/storageUser.ts
@@ -6,3 +6,8 @@ export const USER_STORAGE = '@gymignite:user'
 export async function storageUserSave(user: UserDTO) {
   await AsyncStorage.setItem(USER_STORAGE, JSON.stringify(user))
 }
+export async function storageUserGet() {
+  const storage = await AsyncStorage.getItem(USER_STORAGE)
+  const user: UserDTO = storage ? JSON.parse(storage) : {}
+  return user
+}
