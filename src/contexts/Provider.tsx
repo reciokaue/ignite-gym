600e1042fbd780dd9fdfcd28469af1b5c97f909a
reciokaue <kaue.recio2@gmail.com>
2024-04-11 20:56:01 -0300

    react query

--- src/contexts/Provider.tsx
+++ src/contexts/Provider.tsx
@@ -1,4 +1,5 @@
 import { Loading } from '@components/Loading'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StatusBar } from 'expo-status-bar'
 import { NativeBaseProvider } from 'native-base'
 import { ReactNode } from 'react'
@@ -10,12 +11,21 @@ interface ProviderProps {
   loading?: boolean
   children: ReactNode
 }
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+})
 
 export function Provider({ loading, children }: ProviderProps) {
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar style="light" backgroundColor="transparent" translucent />
-      <AuthProvider>{loading ? children : <Loading />}</AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>{loading ? children : <Loading />}</AuthProvider>
+      </QueryClientProvider>
     </NativeBaseProvider>
   )
 }
