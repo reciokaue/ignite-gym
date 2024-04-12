00aabf7823c1d48ff74c6006646bfb6d0575cc76
reciokaue <kaue.recio2@gmail.com>
2024-04-12 04:47:43 -0300

    getting data on home

---
+++ src/DTOs/exercise.ts
@@ -0,0 +1,7 @@
+export type ExerciseDTO = {
+  id: number
+  name: string
+  series: number
+  repetitions: string
+  group: string
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,61 +2,35 @@ import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
 import { useNavigation } from '@react-navigation/native'
+import { api } from '@services/api'
+import { useQuery } from '@tanstack/react-query'
 import { FlatList, Heading, HStack, Text, VStack } from 'native-base'
 import { useState } from 'react'
+import { ExerciseDTO } from 'src/DTOs/exercise'
 
 export function Home() {
-  const [groups, setGroups] = useState([
-    'costas',
-    'ombro',
-    'bíceps',
-    'triceps',
-    'ombro',
-  ])
-  const [exercises, setExercises] = useState([
-    'Puxada frontal',
-    'Remada cavalinho',
-    'Remada unilateral',
-    'Rosca direta',
-    'Rosca concentrada',
-  ])
   const [selectedGroup, setSelectedGroup] = useState('')
+
+  const { data: groups } = useQuery({
+    queryKey: ['groups'],
+    queryFn: async () => {
+      const response = await api.get(`/groups`)
+      return response.data as string[]
+    },
+  })
+  const { data: exercises } = useQuery({
+    queryKey: ['exercises', selectedGroup],
+    queryFn: async () => {
+      const response = await api.get(`/exercises/bygroup/${selectedGroup}`)
+      console.log(response.data)
+      return response.data as ExerciseDTO[]
+    },
+  })
 
   const navigation = useNavigation()
 
   function handleOpenExerciseDetails() {
     navigation.navigate('exercise')
-  }
-
-  function HeaderContent() {
-    return (
-      <>
-        <FlatList
-          data={groups}
-          renderItem={({ item }) => (
-            <Group
-              key={item}
-              onPress={() => setSelectedGroup(item)}
-              name={item}
-              isActive={selectedGroup.toUpperCase() === item.toUpperCase()}
-            />
-          )}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          my={10}
-          maxH={10}
-          minH={10}
-        />
-        <HStack justifyContent="space-between" mb={5}>
-          <Heading fontFamily="heading" color="gray.200" fontSize="sm">
-            Exercicios
-          </Heading>
-          <Text color="gray.200" fontSize="sm">
-            {exercises.length}
-          </Text>
-        </HStack>
-      </>
-    )
   }
 
   return (
@@ -65,9 +39,36 @@ export function Home() {
       <FlatList
         data={exercises}
         renderItem={({ item }) => (
-          <ExerciseCard onPress={handleOpenExerciseDetails} key={item} />
+          <ExerciseCard onPress={handleOpenExerciseDetails} key={item.id} />
         )}
-        ListHeaderComponent={HeaderContent}
+        ListHeaderComponent={
+          <>
+            <FlatList
+              data={groups}
+              renderItem={({ item }) => (
+                <Group
+                  key={item}
+                  name={item}
+                  onPress={() => setSelectedGroup(item)}
+                  isActive={selectedGroup.toUpperCase() === item.toUpperCase()}
+                />
+              )}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              my={10}
+              maxH={10}
+              minH={10}
+            />
+            <HStack justifyContent="space-between" mb={5}>
+              <Heading fontFamily="heading" color="gray.200" fontSize="sm">
+                Exercicios
+              </Heading>
+              <Text color="gray.200" fontSize="sm">
+                {exercises?.length || ''}
+              </Text>
+            </HStack>
+          </>
+        }
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 10, paddingHorizontal: 24 }}
       />
